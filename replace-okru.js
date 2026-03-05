445b18db04555ae126f5a817a2dc7b2f41fb8c2d
synmaskid <synmaskid@gmail.com>
2026-03-05 20:27:22 +0700

    Update replace-okru.js

--- replace-okru.js
+++ replace-okru.js
@@ -1,50 +1,17 @@
 (function () {
+    const iframes = document.querySelectorAll('iframe');
 
-function replaceOkru(iframe){
+    iframes?.forEach(iframe => {
 
-    if (!iframe.src) return;
+        if (iframe.src?.indexOf('ok.ru') > -1) {
 
-    if (iframe.src.includes("ok.ru")) {
-
-        var match = iframe.src.match(/ok\.ru\/.*?(\d{8,})/i);
-
-        if (match) {
-
-            iframe.src = "https://ssplay.net/ok/" + match[1];
+            iframe.src = iframe.src
+                .replace('ok.ru/videoembed/', 'ssplay.net/ok/')
+                .replace('ok.ru/video/', 'ssplay.net/ok/')
+                .replace('ok.ru/video/editor/', 'ssplay.net/ok/');
 
         }
 
-    }
-
-}
-
-/* chạy khi trang load */
-document.querySelectorAll("iframe").forEach(replaceOkru);
-
-/* theo dõi iframe mới được tạo */
-const observer = new MutationObserver(function(mutations){
-
-    mutations.forEach(function(mutation){
-
-        mutation.addedNodes.forEach(function(node){
-
-            if(node.tagName === "IFRAME"){
-                replaceOkru(node);
-            }
-
-            if(node.querySelectorAll){
-                node.querySelectorAll("iframe").forEach(replaceOkru);
-            }
-
-        });
-
     });
-
-});
-
-observer.observe(document.body,{
-    childList:true,
-    subtree:true
-});
 
 })();
